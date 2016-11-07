9762733b4cd7ca3c76134b30775607c92ce4d77a
Ssenilen <Ssenilen@SSENILEN>
2016-11-07 14:53:14 +0900

    덧셈, 곱셈 함수를 추가했습니다.

--- GitHubTest/GitHubTest/TestHeader.h
+++ GitHubTest/GitHubTest/TestHeader.h
@@ -1,1 +1,11 @@
 #pragma once
+
+int plusnum(int x, int y)
+{
+	return x + y;
+}
+
+int mulnum(int x, int y)
+{
+	return x * y;
+}
--- GitHubTest/GitHubTest/TestSource.cpp
+++ GitHubTest/GitHubTest/TestSource.cpp
@@ -6,4 +6,7 @@ using namespace std;
 int main()
 {
 	cout << "깃허브 프로젝트입니다" << endl;
+
+	int a = 20, b = 30;
+	cout << "a와 b의 합 계산: " << plusnum(a, b) << endl;
 }
